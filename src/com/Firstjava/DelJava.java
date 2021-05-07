bb611ec8ddc08fc8063f3df8debde19d844403c5
nehaagarwal1900 <nehaagarwal1900@gmail.com>
2021-05-07 17:34:17 +0530

    add file

---
+++ src/com/Firstjava/DelJava.java
@@ -0,0 +1,10 @@
+package com.Firstjava;
+
+public class DelJava {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("my first update and changes in files by using intellj");
+    }
+}
