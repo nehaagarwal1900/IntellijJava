e36a52fa3ce06f341db2b9d05bf4e7ca6d2d9e02
nehaagarwal1900 <nehaagarwal1900@gmail.com>
2021-05-10 18:31:38 +0530

    adding new file

--- src/com/Firstjava/Main.java
+++
@@ -1,8 +0,0 @@
-package com.Firstjava;
-
-public class Main {
-
-    public static void main(String[] args) {
-	System.out.println("my first update and changes in files by using intellj");
-    }
-}
